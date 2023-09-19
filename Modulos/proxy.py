821dcd36c620218b5a66ab6721d1db66bc6c58bf
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2023-09-19 15:10:58 -0300

    Update proxy.py

--- Modulos/proxy.py
+++ Modulos/proxy.py
@@ -13,7 +13,7 @@ except:
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
-MSG = '<span style="color:#c33bd2">@</span><span style="color:#8c72d5">s</span><span style="color:#55aad9">c</span><span style="color:#1de1dc">o</span><span style="color:#00ffdf">t</span><span style="color:#29c4e5">t</span><span style="color:#508cec">s</span><span style="color:#7754f4">s</span><span style="color:#9e1cfb">h</span>'
+MSG = ''
 COR = '<font color="null">'
 FTAG = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
